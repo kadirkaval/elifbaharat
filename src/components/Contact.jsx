aee60bda7880d7349c5dab4d42cdd82eafb0bcac
kadir kaval <60962814+kadirkaval@users.noreply.github.com>
2023-02-12 21:20:31 +0300

    signin add to logo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,19 +4,19 @@ import { toast } from "react-toastify";
 import { db } from "../firebase";
 
 export default function Contact({ userRef, listing }) {
-  const [landlord, setLandlord] = useState(null);
+  const [contact, setContact] = useState(null);
   const [message, setMessage] = useState("");
   useEffect(() => {
-    async function getLandlord() {
+    async function getContact() {
       const docRef = doc(db, "users", userRef);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
-        setLandlord(docSnap.data());
+        setContact(docSnap.data());
       } else {
-        toast.error("Could not get landlord data");
+        toast.error("Could not get contact data");
       }
     }
-    getLandlord();
+    getContact();
   }, [userRef]);
 
   function onChange(e) {
@@ -24,10 +24,10 @@ export default function Contact({ userRef, listing }) {
   }
   return (
     <>
-      {landlord !== null && (
+      {contact !== null && (
         <div className="flex flex-col w-full">
           <p>
-            İletişim: <span className="font-semibold">{landlord.name}</span> - Ürün:  {listing.name.toLowerCase()}
+            İletişim: <span className="font-semibold">{contact.name}</span> - Ürün:  {listing.name.toLowerCase()}
           </p>
           <div className="mt-3 mb-6">
             <textarea
@@ -40,7 +40,7 @@ export default function Contact({ userRef, listing }) {
             ></textarea>
           </div>
           <a
-            href={`mailto:${landlord.email}?Subject=${listing.name}&body=${message}`}
+            href={`mailto:${contact.email}?Subject=${listing.name}&body=${message}`}
           >
             <button className="px-7 py-3 bg-blue-600 text-white text-sm uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full text-center mb-6" type="button">Mesaj Gönder</button>
           </a>
